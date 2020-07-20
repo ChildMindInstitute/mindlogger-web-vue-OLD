b3ff4831f539285540061a26ad8e22b55c3e4fe2
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2020-07-20 12:16:53 -0400

    Add sentry logging.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import * as Sentry from '@sentry/browser';
+import { Vue as VueIntegration } from '@sentry/integrations';
+
 import App from './App';
 import router from './router';
 import store from './store/store';
 
+const env = process.env.NODE_ENV || 'development';
+
+if (env !== 'development') {
+  Sentry.init({
+    dsn: 'https://8cd0d02f082f46c7aaccf908df2617a7@o414302.ingest.sentry.io/5320597',
+    integrations: [new VueIntegration({ Vue, attachProps: true })],
+  });
+}
 
 Vue.config.productionTip = false;
 
